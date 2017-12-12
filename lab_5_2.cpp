fed8932f6b5099da54a94b48dacfe44ad64ea47e
Unknown <lovechillyhot@hotmail.com>
2017-12-12 11:00:47 +0700

    แก้ข้อ 2 เสร็จแล้ว

--- lab_5_2.cpp
+++ lab_5_2.cpp
@@ -17,6 +17,7 @@ int main() {
         name1.erase (name1.begin()+10, name1.end());
         cout << "Your name has been changed to " << name1 << endl;
     }
-
+    if (name1.length() < 10)t8
+        cout << name1 << endl;
     return 0;
 }
